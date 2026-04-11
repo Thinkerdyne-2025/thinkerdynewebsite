63604481cfeeff3d76e3e5aab8552be109d4431d
tirugit-2001 <tirumaleshaeyadav@gmail.com>
2026-04-12 01:39:31 +0530

    added cadence software

--- src/components/sections/CaeSoftwares.jsx
+++ src/components/sections/CaeSoftwares.jsx
@@ -1,12 +1,12 @@
-import { useRef } from 'react';
-import { useFadeInUp } from '../../hooks/useScrollAnimation';
+import { useRef } from "react";
+import { useFadeInUp } from "../../hooks/useScrollAnimation";
 import altiumLogo from "../../assets/altium_logo.png";
 import kicadLogo from "../../assets/kicad_logo.png";
-import cadence from "../../assets/candence.jpeg"
+import cadence from "../../assets/candence.jpeg";
 import "./CaeSoftwares.css";
 
 const CAE_LOGOS = [
-  // { name: "Cadence",src:cadence}
+  { name: "Cadence", src: cadence },
   { name: "Altium Designer", src: altiumLogo },
   { name: "KiCad", src: kicadLogo },
 ];
@@ -17,7 +17,9 @@ export default function CaeSoftwares() {
 
   return (
     <section className="cae-softwares" aria-label="CAE Softwares">
-      <h2 className="cae-softwares__title" ref={titleRef}>Engineering Tools & Technologies</h2>
+      <h2 className="cae-softwares__title" ref={titleRef}>
+        Engineering Tools & Technologies
+      </h2>
       {/* <p className="cae-softwares__subtitle">Software We Use</p> */}
       <div className="cae-softwares__logos-wrap">
         <div className="cae-softwares__logos">
